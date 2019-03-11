9dcacf1bc1cdd8ac538270eb0e4f3e8d7b5895bd
Ian Noyes <noyese18@hotmail.com>
2019-03-11 16:16:59 -0700

    Edit form/Import/Save/Encrypt/Decrypt

--- TeamProject/Edit.Designer.cs
+++ TeamProject/Edit.Designer.cs
@@ -31,39 +31,65 @@
             this.txtField = new System.Windows.Forms.TextBox();
             this.btnImport = new System.Windows.Forms.Button();
             this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtField
             // 
-            this.txtField.Location = new System.Drawing.Point(26, 324);
+            this.txtField.Location = new System.Drawing.Point(50, 242);
+            this.txtField.Multiline = true;
             this.txtField.Name = "txtField";
-            this.txtField.Size = new System.Drawing.Size(396, 20);
+            this.txtField.Size = new System.Drawing.Size(299, 98);
             this.txtField.TabIndex = 0;
             // 
             // btnImport
             // 
-            this.btnImport.Location = new System.Drawing.Point(501, 320);
+            this.btnImport.Location = new System.Drawing.Point(371, 271);
             this.btnImport.Name = "btnImport";
             this.btnImport.Size = new System.Drawing.Size(75, 23);
             this.btnImport.TabIndex = 1;
             this.btnImport.Text = "Import";
             this.btnImport.UseVisualStyleBackColor = true;
-            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            this.btnImport.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnEncrypt
             // 
-            this.btnEncrypt.Location = new System.Drawing.Point(346, 367);
+            this.btnEncrypt.Location = new System.Drawing.Point(293, 363);
             this.btnEncrypt.Name = "btnEncrypt";
-            this.btnEncrypt.Size = new System.Drawing.Size(121, 23);
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
             this.btnEncrypt.TabIndex = 2;
-            this.btnEncrypt.Text = "Encrypt/Decrypt";
+            this.btnEncrypt.Text = "Encrypt";
             this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(412, 363);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(507, 270);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // Edit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDecrypt);
             this.Controls.Add(this.btnEncrypt);
             this.Controls.Add(this.btnImport);
             this.Controls.Add(this.txtField);
@@ -79,5 +105,7 @@
         private System.Windows.Forms.TextBox txtField;
         private System.Windows.Forms.Button btnImport;
         private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnSave;
     }
 }
--- TeamProject/Edit.cs
+++ TeamProject/Edit.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace TeamProject
 {
@@ -17,9 +19,51 @@ namespace TeamProject
             InitializeComponent();
         }
 
-        private void btnImport_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            string txt = "";
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open text file";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                string location = theDialog.FileName.ToString();
+                txt = System.IO.File.ReadAllText(location);
+            }
+            txtField.Text = txt;
+        }
 
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            txtField.Text = Encrypt.EncryptString(txtField.Text, "123");
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            txtField.Text = Encrypt.DecryptString(txtField.Text, "123");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            
+            
+                string data = txtField.Text;
+
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "Text File (*.txt)|*.txt";
+                dialog.FileName = "example";
+                //File.WriteAllText(@"C:\test.txt", data);
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Filename includes full path
+                    string fileName = dialog.FileName;
+                    File.WriteAllText(fileName, data);
+                    MessageBox.Show("Data Saved");
+                }
+
+            
         }
     }
 }
