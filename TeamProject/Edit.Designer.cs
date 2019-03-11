4f553e7bcaa381b41d4387004b93c3e3cb4cc82a
Ian Noyes <noyese18@hotmail.com>
2019-03-11 16:25:47 -0700

    Edit Form Design

--- TeamProject/Edit.Designer.cs
+++ TeamProject/Edit.Designer.cs
@@ -33,6 +33,15 @@
             this.btnEncrypt = new System.Windows.Forms.Button();
             this.btnDecrypt = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
+            this.txtCoffee = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdd1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdd2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdd3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSaveCoffee = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtField
@@ -83,11 +92,93 @@
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
+            // txtCoffee
+            // 
+            this.txtCoffee.Location = new System.Drawing.Point(70, 40);
+            this.txtCoffee.Name = "txtCoffee";
+            this.txtCoffee.Size = new System.Drawing.Size(184, 20);
+            this.txtCoffee.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Coffee";
+            // 
+            // txtAdd1
+            // 
+            this.txtAdd1.Location = new System.Drawing.Point(345, 40);
+            this.txtAdd1.Name = "txtAdd1";
+            this.txtAdd1.Size = new System.Drawing.Size(100, 20);
+            this.txtAdd1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(345, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "AddOn 1";
+            // 
+            // txtAdd2
+            // 
+            this.txtAdd2.Location = new System.Drawing.Point(345, 93);
+            this.txtAdd2.Name = "txtAdd2";
+            this.txtAdd2.Size = new System.Drawing.Size(100, 20);
+            this.txtAdd2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(345, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "AddOn 2";
+            // 
+            // txtAdd3
+            // 
+            this.txtAdd3.Location = new System.Drawing.Point(345, 145);
+            this.txtAdd3.Name = "txtAdd3";
+            this.txtAdd3.Size = new System.Drawing.Size(100, 20);
+            this.txtAdd3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(345, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "AddOn 3";
+            // 
+            // btnSaveCoffee
+            // 
+            this.btnSaveCoffee.Location = new System.Drawing.Point(70, 89);
+            this.btnSaveCoffee.Name = "btnSaveCoffee";
+            this.btnSaveCoffee.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveCoffee.TabIndex = 13;
+            this.btnSaveCoffee.Text = "Save Coffee";
+            this.btnSaveCoffee.UseVisualStyleBackColor = true;
+            // 
             // Edit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaveCoffee);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAdd3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAdd2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdd1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCoffee);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnDecrypt);
             this.Controls.Add(this.btnEncrypt);
@@ -107,5 +198,14 @@
         private System.Windows.Forms.Button btnEncrypt;
         private System.Windows.Forms.Button btnDecrypt;
         private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtCoffee;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdd1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdd2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdd3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSaveCoffee;
     }
 }
